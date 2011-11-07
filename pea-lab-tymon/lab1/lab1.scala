af653592d28c65e6fcab062aa0008cf5e0c25bff
Tymon Tobolski <i@teamon.eu>
2011-11-07 11:42:24 +0100

    przeglad zupelny

--- pea-lab-tymon/lab1/lab1.scala
+++ pea-lab-tymon/lab1/lab1.scala
@@ -1,20 +1,46 @@
+case class Task(index: Int, p: Int, d: Int, w: Int)
+
 object Zad1 {
+    def selections[A](list: List[A]): List[(A, List[A])] = list match {
+        case Nil => Nil
+        case x :: xs =>
+            (x, xs) :: (for((y, ys) <- selections(xs)) yield (y, x :: ys))
+    }
+
+    def permute[A](list: List[A]): List[List[A]] = list match {
+        case Nil => List(Nil)
+        case x :: Nil => List(list)
+        case x :: y :: Nil => List(list, List(y, x))
+        case _ => for {
+            (y, ys) <- selections(list)
+            ps <- permute(ys)
+        } yield y :: ps
+    }
+
+    def cost(list: List[Task]) = {
+        (list.foldLeft((0,0)){
+            case ((time, cost), x) => (time+x.p, cost + Math.max(0, (time+x.p) - x.d) * x.w)
+        })._2
+    }
+
     def main(args: Array[String]) {
         if(args.length == 1){
             val n = args(0).toInt
-            val (p, d, w) = io.Source.fromFile("%d.txt" format n).getLines.map { _.split(" ").map(_.toInt) }.foldRight(List[Int](), List[Int](), List[Int]()){
-                case (x, (p1, d1, w1)) => (x(0) :: p1, x(1) :: d1, x(2) :: w1)
-            }
-
             println("Zadanie %d" format n)
             println()
             println("    P |     D |     W")
             println("---------------------")
+            val tasks = io.Source.fromFile("data/%d.txt" format n).getLines.zipWithIndex.map {
+                case (line, index) =>
+                    val xs = line.split(" ").map(_.toInt)
+                    Task(index+1, xs(0), xs(1), xs(2))
+            }.toList
 
-            p.zip(d).zip(w) foreach {
-                case ((p1, d1), w1) => println("%5d | %5d | %5d".format(p1, d1, w1))
-            }
+            tasks map { t => "%5d | %5d | %5d".format(t.p, t.d, t.w) } foreach println
 
+            val best = permute(tasks).map {t => (t, cost(t)) }.minBy(_._2)
+            println(best._2)
+            println(best._1.map(_.index))
         } else {
             println("Podaj liczbe");
         }
