68f11aea93a721cdb2118ad716962c1e582d4854
Tymon Tobolski <i@teamon.eu>
2011-11-11 22:20:04 +0100

    db fix

--- bazy-danych-projekt/webapp/app/models/db/project.rb
+++ bazy-danych-projekt/webapp/app/models/db/project.rb
@@ -7,32 +7,8 @@ class Java::Db::Project
   def tasks
     ListProxy.new(self, :tasks, :project, Db::Task)
   end
-end
 
-class ListProxy
-  def initialize(parent, coll, pname, klazz)
-    @parent = parent
-    @coll = coll
-    @pname = pname
-    @klazz = klazz
-  end
-
-  def method_missing(name, *args, &block)
-    __coll.send(name, *args, &block)
-  end
-
-  def find(id)
-    __coll.find {|e| e.id == id.to_i }
-  end
-
-  def create(attrs = {})
-    obj = @klazz.new(attrs.merge(@pname => @parent))
-    __coll << obj
-    obj.save
-    obj
-  end
-
-  def __coll
-    @parent.send("get_#{@coll}")
+  def milestones
+    ListProxy.new(self, :milestones, :project, Db::Milestone)
   end
 end
