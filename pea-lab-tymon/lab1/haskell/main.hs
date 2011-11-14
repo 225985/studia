133f4f231c709e4385179893db8d7a354a89bb12
Tymon Tobolski <i@teamon.eu>
2011-11-14 04:31:01 +0100

    pea

--- pea-lab-tymon/lab1/haskell/main.hs
+++ pea-lab-tymon/lab1/haskell/main.hs
@@ -1,5 +1,5 @@
 import Data.List
-import Data.Function.Memoize
+import qualified Data.Text as T
 import System (getArgs)
 import Debug.Trace (trace)
 
@@ -8,41 +8,33 @@ data Task = Task {index::Int, p::Int, d::Int, w::Int}
 instance Show Task where
     show t = show $ index t
 
-
--- showTask :: Task -> String
+showTask :: Task -> String
 showTask t = show [index t, p t, d t, w t]
 
+readTask :: String -> Int -> Task
+readTask s i = Task {index=i, p=x, d=y, w=z} where (x:y:z:[]) = map read $ words s
 
--- readTask :: String -> Int -> Task
-readTask s i = case (map read (words s)) of x:y:z:[] -> Task {index=i, p=x, d=y, w=z}
-
-
--- selections :: [a] -> [(a, [a])]
+selections :: [a] -> [(a, [a])]
 selections []       = []
 selections (x:xs)   = (x,xs) : [(y, x:ys) | (y,ys) <- selections xs]
 
-
--- permute :: [a] -> [[a]]
+permute :: [a] -> [[a]]
 permute []  = [[]]
 permute xs  = [y:ps | (y,ys) <- selections xs, ps <- permute ys]
 
-
--- cost :: [Task] -> Int
+cost :: [Task] -> Int
 cost tasks = let c1 (time, cost) task = (newTime, cost + (max 0 (newTime - d task)) * w task)
                     where newTime = time + p task
-             in snd (foldl c1 (0,0) tasks)
+             in snd (foldl' c1 (0,0) tasks)
 
-
--- minimumCost :: [[Task]] -> ([Task], Int)
+minimumCost :: [[Task]] -> ([Task], Int)
 minimumCost tasks = minimumBy compareSnd $ map (\x -> (x,cost x)) tasks
     where compareSnd = \x y -> compare (snd x) (snd y)
 
-
--- bruteforce :: [Task] -> ([Task], Int)
+bruteforce :: [Task] -> ([Task], Int)
 bruteforce = minimumCost . permute
 
-
--- eliminate :: ([Task] -> Bool) -> [Task] -> ([Task], Int)
+eliminate :: ([Task] -> Bool) -> [Task] -> ([Task], Int)
 eliminate p tasks = minimumCost $ branch [] tasks []
       where branch :: [Task] -> [Task] -> [[Task]] -> [[Task]]
             branch xs [] qs              = xs : qs
@@ -50,37 +42,44 @@ eliminate p tasks = minimumCost $ branch [] tasks []
                 where fm (z, zs) = branch (xs ++ [z]) zs qs
             branch _  _  qs              = qs
 
-
--- oneone :: [Task] -> ([Task], Int)
-oneone tasks = eliminate (\xs -> cost xs >= lb) tasks
+elimination1 :: [Task] -> ([Task], Int)
+elimination1 tasks = eliminate (\xs -> cost xs >= lb) tasks
     where lb = cost tasks
 
+elimination2 :: [Task] -> ([Task], Int)
+elimination2 = eliminate (\xs -> anySwap (\ys -> cost ys < cost xs) xs)
 
--- twotwo :: [Task] -> ([Task], Int)
-twotwo = eliminate (\xs -> cost (swap xs) < cost xs)
-
-
--- run :: String -> ([Task] -> ([Task], Int)) -> IO ()
+run :: String -> ([Task] -> ([Task], Int)) -> IO ()
 run n f = do
     x <- readFile ("../data/" ++ n ++ ".txt")
-    print $ f $ map (uncurry readTask) (zip (lines x) [1..] )
+    let tasks = map (uncurry readTask) (zip (lines x) [1..] )
 
+    putStrLn "   P    D    W"
+    putStrLn "--------------"
+    putStrLn $ unlines $ map (\task -> unwords $ map (\e -> T.unpack $ T.justifyRight 4 ' ' (T.pack $ show e)) [p task, d task, w task]) tasks
+
+    let (res, cost) = f tasks
+    putStrLn $ "Naljepsze uszeregowanie: " ++ (show res)
+    putStrLn $ "                  Koszt: " ++ (show cost)
 
 -- swap [x1, x2, ... , xn, xm] into [x1, x2, ... , xm, xn]
--- swap :: [a] -> [a]
+swap :: [a] -> [a]
 swap []     = []
 swap [x]    = [x]
 swap list   = case (reverse list) of x:y:xs -> reverse (y:x:xs)
 
+swaps :: [a] -> [[a]]
+swaps [] = []
+swaps [x] = [[x]]
+swaps (x:y:[]) = [[y,x]]
+swaps (x:y:xs) = (y:x:xs) : map (y:) (swaps (x:xs))
+
+anySwap :: ([a] -> Bool) -> [a] -> Bool
+anySwap f xs = any f $ map reverse $ swaps $ reverse xs
 
 main = do
     args <- System.getArgs
     case args of    "0":n:[] -> run n bruteforce
-                    "1":n:[] -> run n oneone
-                    "2":n:[] -> run n twotwo
-                    "x":n:[] -> do
-                            run n bruteforce
-                            run n oneone
-                            run n twotwo
-                    _        -> error "Usage: main [0-2 | x] [n]"
-
+                    "1":n:[] -> run n elimination1
+                    "2":n:[] -> run n elimination2
+                    _        -> error "Usage: Main [0-2] [n]"
--- pea-lab-tymon/lab1/haskell/test.rb
+++ pea-lab-tymon/lab1/haskell/test.rb
@@ -1,11 +1,13 @@
 require 'benchmark'
 
+opts = "+RTS -K1024000000 -RTS"
+
 Benchmark.bm do |x|
   [5,6,8,9,10,11,12,13,15,17,18].each do |i|
     res = []
-    x.report("brute #{"%2d" % i}"){ res << `./Main 0 #{i}` }
-    x.report("one   #{"%2d" % i}"){ res << `./Main 1 #{i}` }
-    x.report("two   #{"%2d" % i}"){ res << `./Main 2 #{i}` }
+    # x.report("brute #{"%2d" % i}"){ res << `./Main #{opts} 0 #{i}` }
+    # x.report("one   #{"%2d" % i}"){ res << `./Main #{opts} 1 #{i}` }
+    x.report("two   #{"%2d" % i}"){ res << `./Main #{opts} 2 #{i}` }
     puts res
   end
 end
