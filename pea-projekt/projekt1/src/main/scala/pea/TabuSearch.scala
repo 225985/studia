10e1a39b941f35c8939955bf21057c3ebfcc4443
Tymon Tobolski <i@teamon.eu>
2011-12-14 21:23:05 +0100

    Tabu

--- pea-projekt/projekt1/src/main/scala/pea/TabuSearch.scala
+++ pea-projekt/projekt1/src/main/scala/pea/TabuSearch.scala
@@ -6,14 +6,13 @@ abstract class TabuSearch[A, R : Ordering]{
     def N: Int
 
     def F(x: A): R  // cost function
-    def S(x: A): Traversable[A]  // new states generator
+    def S(x: A): TraversableOnce[A]  // new states generator
 
     def apply(s0: A) = {
         def inner(oldState: A, n: Int): A = {
             if(n <= 0) oldState
             else {
-                val newStates = S(oldState)
-                val (bestState, cost) = newStates.map { s => (s, F(s)) }.minBy(_._2)
+                val (bestState, cost) = S(oldState).map { s => (s, F(s)) }.minBy(_._2)
                 if(cost < F(oldState)) inner(bestState, n-1)
                 else inner(oldState, n-1)
             }
