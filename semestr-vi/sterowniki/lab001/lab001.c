18cc41a0b0c203f488efa3b0a5738b246c4d061a
Tymon Tobolski <i@teamon.eu>
2012-03-04 23:08:57 +0100

    sterowniki lab1 spr

--- semestr-vi/sterowniki/lab001/lab001.c
+++ semestr-vi/sterowniki/lab001/lab001.c
@@ -9,13 +9,15 @@
 #define RANDSET(port, pin) (rand() > RAND_MAX / 2) ? (SET(port, pin)) : (CLR(port, pin))
 
 int main(void){
+    // Ustawienie DDRD jako wyjscie
     DDRD |= _BV(PD2) | _BV(PD3) | _BV(PD4) | _BV(PD5);
 
+    // Ustawienie DDRB jako wejscie
     DDRB = 0x00;
-    SET(PORTB, PB0);
+    SET(PORTB, PB0); // pull-up na PB0
 
     while(1){
-        if( (PINB & _BV(PB0)) != 0) {
+        if((PINB & _BV(PB0)) != 0) { // sprawdzenie stanu wejscia PB0
             RANDSET(PORTD, PD2);
             RANDSET(PORTD, PD3);
             RANDSET(PORTD, PD4);
