f1df6a2feb33a6b35ea920800690256ea1e12747
Jacek Wieczorek <me@jacbar.pl>
2012-03-04 22:33:49 +0100

    Add webapp

---
+++ webapp/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Webapp::Application.config.secret_token = '1c6fdebb97766321df906c05e82c8ecd4df89a3e5cba5dca48b6860826223a164d24bbc0c43ab6e9bd9cc964b9b3838026e8b8507e9c9741898b02176b4aa133'
