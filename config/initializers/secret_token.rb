8103a7e6e02b8f75a19508ef35cd40c48c38bd26
Mateusz Lenik <mt.lenik@gmail.com>
2012-03-13 13:01:32 +0100

    App

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mandelbrot::Application.config.secret_token = '15ac483fdaff8e1621a7a282201953c1ac0d343abc36cafe7712e9c5f3b3124c372442e74cbd7d218c71b2624bfe8dda608d6bc75e08807b20f2dcdf6c36696a'
