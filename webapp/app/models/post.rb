e9040edd14ecf1bdb24b9e2904e3eecb4b588b50
Tymon Tobolski <i@teamon.eu>
2012-04-01 20:28:41 +0200

    Fixed post.rb

--- webapp/app/models/post.rb
+++ webapp/app/models/post.rb
@@ -1,10 +1,11 @@
 class Post < ActiveRecord::Base
-	belongs_to :user, :blog
+  belongs_to :user
+  belongs_to :blog
 
-	attr_accessible :content, :user_id
+  attr_accessible :content, :user_id
 
-	validates :user_id, presence: true
-	validates :content, presence: true, length: { maximum: 140 }
-	
-	default_scope order: 'microposts.created_at DESC'
+  validates :user_id, presence: true
+  validates :content, presence: true, length: { maximum: 140 }
+
+  default_scope order: 'posts.created_at DESC'
 end
