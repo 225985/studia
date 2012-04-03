012b6613d00f9b4a001d8a7e78180918488d8f07
Jacek Wieczorek <me@jacbar.pl>
2012-04-03 22:35:34 +0200

    fix

--- webapp/app/controllers/posts_controller.rb
+++ webapp/app/controllers/posts_controller.rb
@@ -18,7 +18,7 @@ class PostsController < ApplicationController
 
 
   def destroy
-    @post = @blog.post.find(params[:id])
+    @post = @blog.posts.find(params[:id])
     @post.destroy
     respond_to do |format|
       format.html { redirect_to blog_path(@blog) }
