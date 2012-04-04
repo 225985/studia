b3d7d70ca70ca080fae9b754e05c6d7b445757fa
Tymon Tobolski <i@teamon.eu>
2012-04-04 23:43:26 +0200

    Web API

--- dist/src/main/scala/Base.scala
+++ dist/src/main/scala/Base.scala
@@ -5,18 +5,24 @@ import akka.routing.FromConfig
 import akka.event._
 import com.typesafe.config._
 
+import akka.pattern.ask
+import akka.util.Timeout
+import akka.util.duration._
+import akka.dispatch.Await
+
 case class Job(item: String)
+case class JobResult(count: Int)
+
 case class PartialJob(item: String)
-case class Result(item: String, count: Int)
+case class PartialJobResult(item: String, count: Int)
 
 class WorkerActor extends Actor with ActorLogging {
   def receive = {
     case PartialJob(item) =>
       log.info("Working on: " + item)
-      sender ! Result(item, item.length)
+      sender ! PartialJobResult(item, item.length)
   }
 }
-
 
 object Worker {
   def main(args: Array[String]) {
@@ -29,29 +35,23 @@ class Worker(config: Config) {
   val system = ActorSystem("Worker", config)
 }
 
+
 class BaseActor(router: ActorRef) extends Actor with ActorLogging {
   def receive = {
     case Job(item) =>
       log.info("Distributing job: " + item)
-      item.split(" ").foreach { item => router ! PartialJob(item) }
+      implicit val timeout = Timeout(5 seconds)
+      val count = item.split("-").map { item =>
+        router ? PartialJob(item)
+      }.map { future =>
+        Await.result(future, timeout.duration).asInstanceOf[PartialJobResult]
+      }.foldLeft(0) {
+        case (sum, res) => sum + res.count
+      }
 
-    case Result(item, count) =>
+      sender ! JobResult(count)
+
+    case PartialJobResult(item, count) =>
       log.info("Got result for " + item + " => " + count)
-  }
-}
-
-object Base {
-  def main(args: Array[String]) = {
-    val config = ConfigFactory.load()
-
-    // Just for tests
-    // new Worker(config.getConfig("worker1").withFallback(config))
-    // new Worker(config.getConfig("worker2").withFallback(config))
-    // --
-
-    val system = ActorSystem("Base", config.getConfig("base").withFallback(config))
-    val router = system.actorOf(Props[WorkerActor].withRouter(FromConfig()), name = "router")
-    val base = system.actorOf(Props(new BaseActor(router)), name = "base")
-    base ! Job("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus imperdiet laoreet orci, in laoreet nunc malesuada vitae. Curabitur sit amet arcu elit. Donec luctus pellentesque dui, at dignissim odio tempor et. Integer porttitor neque vel nunc vehicula tincidunt. In dapibus lacinia dui at vulputate. Quisque venenatis bibendum eros, quis scelerisque ipsum placerat id. Nullam mi sem, iaculis ut mollis quis, condimentum non ipsum. Pellentesque neque felis, mollis sit amet pellentesque quis, lobortis ac orci. Mauris pretium nulla a est dapibus convallis. Aenean eget tellus quam. Fusce cursus vestibulum nibh auctor pretium. Donec ultricies tortor non nibh pellentesque volutpat. Curabitur aliquam, lorem ac fermentum eleifend, tortor nisi rutrum ligula, ut dapibus mi velit in mi. Nulla dapibus, lorem et dignissim ultricies, lacus felis tincidunt ante, ut rutrum diam enim sed urna. Fusce egestas cursus euismod.")
   }
 }
---
+++ dist/src/main/scala/Web.scala
@@ -0,0 +1,47 @@
+package dist
+
+import com.typesafe.config._
+import akka.routing.FromConfig
+
+import com.typesafe.play.mini._
+import play.api.mvc._
+import play.api.mvc.Results._
+import play.api.libs.concurrent._
+import play.api.Play.current
+
+import akka.actor._
+import akka.pattern.ask
+import akka.util.Timeout
+import akka.util.duration._
+
+import scala.collection.JavaConversions._
+
+object Hardcore {
+  lazy val config = ConfigFactory.load()
+
+  lazy val system = ActorSystem("Base", config.getConfig("base"))
+  lazy val router = system.actorOf(Props[WorkerActor].withRouter(FromConfig()), name = "router")
+
+  lazy val base = {
+    // Just for tests
+    new Worker(config.getConfig("worker1"))
+    new Worker(config.getConfig("worker2"))
+
+    system.actorOf(Props(new BaseActor(router)), name = "base")
+  }
+}
+
+
+object Web extends Application {
+  implicit val timeout = Timeout(5 seconds)
+
+  def route = {
+    case GET(Path("/")) & QueryString(qs) => Action {
+      val sentence = QueryString(qs, "s").flatMap(_.headOption).getOrElse("")
+
+      Async {
+        (Hardcore.base ? Job(sentence)).asPromise.map { case JobResult(count) => Ok("Count: " + count + "\n") }
+      }
+    }
+  }
+}
