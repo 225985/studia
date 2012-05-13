3e2ffe1a711a1a30ca2ac99050d65a29f2044c7e
Tymon Tobolski <i@teamon.eu>
2012-05-13 15:18:34 +0200

    Updated webapp production conf

--- webapp/config/environments/production.rb
+++ webapp/config/environments/production.rb
@@ -12,7 +12,10 @@ Mandelbrot::Application.configure do
   config.serve_static_assets = false
 
   # Compress JavaScripts and CSS
-  config.assets.compress = true
+  config.assets.compress = false
+
+  config.assets.css_compressor  = :scss
+  config.assets.js_compressor   = :uglifier
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
   config.assets.compile = false
