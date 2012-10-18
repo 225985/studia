ff3f3716ada2a5ae09ac9ae99256a4288fc30dbc
Tymon Tobolski <i@teamon.eu>
2012-10-18 21:10:26 +0200

    SSO lab 3.1

---
+++ semestr-vii/sso-tymon/lab3/zad1/main.c
@@ -0,0 +1,53 @@
+/*
+ * SSO - Lab 3.1
+ * Tymon Tobolski 181037
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+int main(int argc, const char *argv[]){
+    int fd[2];
+    int pid, i, n, status;
+    char inbuf[10];
+    char outbuf[10];
+
+    if(argc < 2){
+        printf("Usage: %s num\n", argv[0]);
+        exit(-1);
+    }
+
+    n = atoi(argv[1]);
+
+    pipe(fd);
+
+    if((pid = fork()) == 0){
+        close(fd[1]);
+
+        for(i=0; i < n; ++i){
+            read(fd[0], outbuf, 20);
+            printf("[\033[1;31mc\033[0m] Got %s\n", outbuf);
+            fflush(stdout);
+            sleep(1);
+        }
+
+        close(fd[0]);
+    } else {
+        close(fd[0]);
+
+        for(i=0; i<n; ++i){
+            sprintf(inbuf, "%d", 100*i);
+            printf("[\033[1;32mp\033[0m] Sending %s\n", inbuf);
+            fflush(stdout);
+            write(fd[1], inbuf, 20);
+            /*sleep(1);*/
+        }
+
+        close(fd[1]);
+    }
+
+    wait(&status);
+
+    return 0;
+}
