82fe63de49829861126b35af097a89e8adb169bb
Jacek Wieczorek <me@jacbar.pl>
2012-10-21 16:31:02 +0200

    zad3

--- semestr-vii/sso-jacek/lab3/zad3/zad3.c
+++ semestr-vii/sso-jacek/lab3/zad3/zad3.c
@@ -9,22 +9,32 @@
 
 int main(int argc, const char *argv[]){
     int fd1[2], fd2[2];
-	int pid, status, i, res;
-	char buffer[255];
+    int pid, status, res;
+    char buffer[255];
+
+    char commands[4][100];
+
+    if(argc < 5) {
+        printf("Wrong usage! : ./zad3 cmd1 -par1 cmd2 -par1 \n");
+        return -1;
+    }
+
 
     pipe(fd1);
     pipe(fd2);
     
+
+
 
     if((pid = fork()) == 0){
         close(fd1[0]);
         close(fd2[0]);
         close(fd2[1]);
 
-        printf("ls -l \n");
+        printf("Command 1 : %s %s \n", argv[1], argv[2]);
         dup2(fd1[1], 1); // 1 - stdout 
 
-        execlp("ls", "l", "-l", NULL);
+        execlp(argv[1], "cmd1", argv[2], NULL);
         close(fd1[1]);
     }
     
@@ -33,11 +43,11 @@ int main(int argc, const char *argv[]){
         close(fd1[1]);
         close(fd2[1]);
 
-        printf("cat -n \n");
+        printf("Command 2 : %s %s\n", argv[3], argv[4]);
         dup2(fd2[0], 0); // 0 - stdin
 
 
-        execlp("cat", "c", "-n", NULL);
+        execlp(argv[3], "c", argv[4], NULL);
         close(fd2[0]);
     }
 
